4f2b57ff318834af98f4b27453d6cf6624e0b8fb
Jaap Versteegh <j.r.versteegh@orca-st.com>
2018-09-18 18:51:41 +0200

    Progress with server implementation

--- include/modbus/server.hpp
+++ include/modbus/server.hpp
@@ -29,6 +29,7 @@
 #include <string>
 #include <array>
 #include <cstdint>
+#include <ostream>
 
 #include <boost/bind.hpp>
 #include <boost/shared_ptr.hpp>
@@ -53,161 +54,173 @@ using tcp = boost::asio::ip::tcp;
 
 
 template <typename ServerHandler>
-    struct server;
+struct server;
 
 template <typename ServerHandler>
-    struct tcp_connection: public boost::enable_shared_from_this<tcp_connection<ServerHandler> > {
-        typedef boost::shared_ptr<tcp_connection<ServerHandler> > pointer;
+struct tcp_connection: public boost::enable_shared_from_this<tcp_connection<ServerHandler> > {
+	typedef boost::shared_ptr<tcp_connection<ServerHandler> > pointer;
 
-        static pointer create(asio::io_service& io_service, const boost::shared_ptr<ServerHandler>& handler) {
-            return pointer(new tcp_connection<ServerHandler>(io_service, handler));
-        }
+	static pointer create(asio::io_service& io_service, const boost::shared_ptr<ServerHandler>& handler) {
+		return pointer(new tcp_connection<ServerHandler>(io_service, handler));
+	}
 
-        tcp::socket& socket() {
-            return socket_;
-        }
+	tcp::socket& socket() {
+		return socket_;
+	}
 
-        void start(asio::yield_context yield) {
-            while (socket_.is_open()) {
-                read_data(yield);
-            }
-        }
+	void start(asio::yield_context yield) {
+		while (socket_.is_open()) {
+			read_data(yield);
+		}
+	}
 
 
-        private:
-        tcp::socket socket_;
-        boost::shared_ptr<ServerHandler> handler_;
-        asio::io_service& io_service_;
+private:
+	tcp::socket socket_;
+	boost::shared_ptr<ServerHandler> handler_;
+	asio::io_service& io_service_;
 
-        tcp_connection(asio::io_service& io_service, const boost::shared_ptr<ServerHandler>& handler): 
-            socket_(io_service), handler_(handler), io_service_(io_service) {};
+	tcp_connection(asio::io_service& io_service, const boost::shared_ptr<ServerHandler>& handler): 
+		socket_(io_service), handler_(handler), io_service_(io_service) {};
 
-        void read_data(asio::yield_context yield) {
-            asio::streambuf read_buffer;
-            size_t bytes_transferred = socket_.async_read_some(read_buffer.prepare(1024), yield);
-            read_buffer.commit(bytes_transferred);
-            while (read_buffer.size() > 0) {
-                handle_read(read_buffer, yield);
-            }
-        }
+	void read_data(asio::yield_context yield) {
+		asio::streambuf read_buffer;
+		size_t bytes_transferred = socket_.async_read_some(read_buffer.prepare(1024), yield);
+		read_buffer.commit(bytes_transferred);
+		while (read_buffer.size() > 0) {
+			handle_read(read_buffer, yield);
+		}
+	}
 
-        void handle_read(asio::streambuf& read_buffer, asio::yield_context yield) {
-            boost::system::error_code error;
-            tcp_mbap header;
+	void handle_read(asio::streambuf& read_buffer, asio::yield_context yield) {
+		boost::system::error_code error;
+		tcp_mbap header;
 
-            const uint8_t* data = boost::asio::buffer_cast<const uint8_t*>(read_buffer.data());
-            data = impl::deserialize(data, read_buffer.size(), header, error);
-            if (error) {
-                read_buffer.consume(read_buffer.size());
-                return;
-            }
-            read_buffer.consume(header.size());
-            if (header.length < 2) {
-                write_error(header, errc::illegal_function, yield);
-                return;
-            }
-            size_t data_size = static_cast<size_t>(header.length - 1);
+		const uint8_t* data = boost::asio::buffer_cast<const uint8_t*>(read_buffer.data());
+		data = impl::deserialize(data, read_buffer.size(), header, error);
+		if (error) {
+			read_buffer.consume(read_buffer.size());
+			return;
+		}
+		read_buffer.consume(header.size());
+		if (header.length < 2) {
+			write_error(header, errc::illegal_function, yield);
+			return;
+		}
+		size_t data_size = static_cast<size_t>(header.length - 1);
 
-            if (read_buffer.size() >= data_size) {
-                try {
-                    handle_data(data, data_size, yield);
-                }
-                catch (modbus_exception& e) {
-                }
-            }
-            else {
-                write_error(header, errc::illegal_data_value, yield);
-            }
-            read_buffer.consume(data_size);
-        }
+		if (read_buffer.size() >= data_size) {
+			try {
+				handle_data(header, data, data_size, yield);
+			}
+			catch (modbus_exception& e) {
+				write_error(header, e.get_error(), yield);
+			}
+		}
+		else {
+			write_error(header, errc::illegal_data_value, yield);
+		}
+		read_buffer.consume(data_size);
+	}
 
-        void handle_data(const uint8_t* data, size_t data_size, asio::yield_context yield) {
-            switch (*data) {
-                case functions::read_coils:
-                    request::read_coils req;
-                    data = impl::deserialize(data, data_size, req); 
-                    break;
-                case functions::read_discrete_inputs:;
-                case functions::read_holding_registers:;
-                case functions::read_input_registers:;
-                case functions::write_single_coil:;
-                case functions::write_single_register:;
-                case functions::write_multiple_coils:;
-                case functions::write_multiple_registers:;
-                default:
-                                                         throw modbus_exception(errc::illegal_function); 
-            }
-        }
+	void handle_data(tcp_mbap header, const uint8_t* data, size_t data_size, asio::yield_context yield) {
+		asio::streambuf write_buffer;
+		auto out = std::ostreambuf_iterator<char>(&write_buffer);
+		switch (*data) {
+			case functions::read_coils: {
+				request::read_coils req;
+				impl::deserialize(data, data_size, req); 
+				auto response = handler_->handle_read_coils(req);
+				impl::serialize(out, header); 
+				impl::serialize(out, response);
+				asio::async_write(socket_, write_buffer, yield);
+				break;
+			}
+			case functions::read_discrete_inputs:;
+			case functions::read_holding_registers:;
+			case functions::read_input_registers:;
+			case functions::write_single_coil:;
+			case functions::write_single_register:;
+			case functions::write_multiple_coils:;
+			case functions::write_multiple_registers:;
+			default:
+				 throw modbus_exception(errc::illegal_function); 
+		}
+	}
 
-        void write_error(tcp_mbap header, errc::errc_t error, asio::yield_context yield) {
-        }
+	void write_error(tcp_mbap header, errc::errc_t error, asio::yield_context yield) {
+	}
 
-    };
+};
 
 
 struct default_handler {
-    default_handler()
-        : registers_(16 * 1024) {}
+	default_handler()
+		: registers_(16 * 1024) {}
 
-    response::write_multiple_registers handle_multiple_write_registers(request::write_multiple_registers& req) {
-        response::write_multiple_registers resp;
-        resp.address = req.address;
-        resp.count = 0;
-        auto iit = req.values.begin();
-        auto oit = registers_.begin() + req.address;
-        while (iit < req.values.end() && oit < registers_.end()) {
-            *oit++ = *iit++;
-            resp.count++;
-        }
-        return resp;
-    }
-    response::read_holding_registers handle_read_holding_registers(request::read_holding_registers& req) {
-        response::read_holding_registers resp;
-        int count = 0;
-        auto it = registers_.cbegin() + req.address;
-        while (count < req.count && it < registers_.end()) {
-            resp.values.push_back(*it++);
-            count++;
-        }
-        return resp;
-    }
-    private:
-    std::vector<std::uint16_t> registers_;
+	response::write_multiple_registers handle_multiple_write_registers(request::write_multiple_registers& req) {
+		response::write_multiple_registers resp;
+		resp.address = req.address;
+		resp.count = 0;
+		auto iit = req.values.begin();
+		auto oit = registers_.begin() + req.address;
+		while (iit < req.values.end() && oit < registers_.end()) {
+			*oit++ = *iit++;
+			resp.count++;
+		}
+		return resp;
+	}
+	response::read_holding_registers handle_read_holding_registers(request::read_holding_registers& req) {
+		response::read_holding_registers resp;
+		int count = 0;
+		auto it = registers_.cbegin() + req.address;
+		while (count < req.count && it < registers_.end()) {
+			resp.values.push_back(*it++);
+			count++;
+		}
+		return resp;
+	}
+	response::read_coils handle_read_coils(request::read_coils& req) {
+		response::read_coils resp;
+		return resp;
+	}
+private:
+	std::vector<std::uint16_t> registers_;
 };
 
 // A Modbus server base class
 template <typename ServerHandler>
 struct server: public boost::enable_shared_from_this<server<ServerHandler> > {
-    server(asio::io_service& io_service, boost::shared_ptr<ServerHandler>& handler): 
-        acceptor_(io_service, tcp::endpoint(tcp::v4(), 502)),
-        handler_(handler) {
-            start_accept();
-        }
+	server(asio::io_service& io_service, boost::shared_ptr<ServerHandler>& handler): 
+		acceptor_(io_service, tcp::endpoint(tcp::v4(), 502)),
+		handler_(handler) {
+			start_accept();
+		}
 
-    protected:
+	protected:
 
-    private:
-    void start_accept() {
-        auto  new_connection = tcp_connection<ServerHandler>::create(acceptor_.get_io_service(), handler_);
+	private:
+	void start_accept() {
+		auto  new_connection = tcp_connection<ServerHandler>::create(acceptor_.get_io_service(), handler_);
 
-        acceptor_.async_accept(
-                new_connection->socket(),
-                boost::bind(&server<ServerHandler>::handle_accept, this, new_connection, asio::placeholders::error)
-                );
-    }
+		acceptor_.async_accept(
+				new_connection->socket(),
+				boost::bind(&server<ServerHandler>::handle_accept, this, new_connection, asio::placeholders::error)
+				);
+	}
 
-    void handle_accept(typename tcp_connection<ServerHandler>::pointer new_connection,
-            const boost::system::error_code& error) {
-        if (!error)
-        {
-            asio::spawn(acceptor_.get_io_service(), boost::bind(&tcp_connection<ServerHandler>::start, new_connection, _1));
-        }
+	void handle_accept(typename tcp_connection<ServerHandler>::pointer new_connection,
+			const boost::system::error_code& error) {
+		if (!error)
+		{
+			asio::spawn(acceptor_.get_io_service(), boost::bind(&tcp_connection<ServerHandler>::start, new_connection, _1));
+		}
 
-        start_accept();
-    }
+		start_accept();
+	}
 
-    tcp::acceptor acceptor_;
-    boost::shared_ptr<ServerHandler> handler_;
+	tcp::acceptor acceptor_;
+	boost::shared_ptr<ServerHandler> handler_;
 };
 
 }
